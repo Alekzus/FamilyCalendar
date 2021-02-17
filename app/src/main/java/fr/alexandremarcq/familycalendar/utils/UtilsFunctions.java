481056975774f54c62d42a9e2fe9f32fc7131065
alexandremarcq <alexandremarcq58@gmail.com>
2021-02-17 10:41:26 +0100

    Added year as a return value

--- app/src/main/java/fr/alexandremarcq/familycalendar/utils/UtilsFunctions.java
+++ app/src/main/java/fr/alexandremarcq/familycalendar/utils/UtilsFunctions.java
@@ -5,10 +5,11 @@ import java.util.Locale;
 
 public abstract class UtilsFunctions {
     public static int[] dateConverter(long milliSeconds) {
-        String dateString = new SimpleDateFormat("ddMM", Locale.getDefault())
+        String dateString = new SimpleDateFormat("ddMMyyyy", Locale.getDefault())
                 .format(milliSeconds);
         int day = Integer.parseInt(dateString.substring(0, 2));
-        int month = Integer.parseInt(dateString.substring(2)) - 1;
-        return new int[]{day, month};
+        int month = Integer.parseInt(dateString.substring(2, 4)) - 1;
+        int year = Integer.parseInt(dateString.substring(4));
+        return new int[]{day, month, year};
     }
 }
