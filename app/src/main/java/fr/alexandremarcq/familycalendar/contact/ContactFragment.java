0687640cc007fc7552c1558d27ad604c81ddd3f5
alexandremarcq <alexandremarcq58@gmail.com>
2021-03-02 18:08:10 +0100

    Removed unused imports

--- app/src/main/java/fr/alexandremarcq/familycalendar/contact/ContactFragment.java
+++ app/src/main/java/fr/alexandremarcq/familycalendar/contact/ContactFragment.java
@@ -1,20 +1,17 @@
 package fr.alexandremarcq.familycalendar.contact;
 
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import fr.alexandremarcq.familycalendar.R;
+import fr.alexandremarcq.familycalendar.database.CalendarDatabase;
 import fr.alexandremarcq.familycalendar.databinding.FragmentContactBinding;
+import fr.alexandremarcq.familycalendar.utils.ViewModelFactory;
 
 public class ContactFragment extends Fragment {
 
@@ -28,10 +25,11 @@ public class ContactFragment extends Fragment {
         mBinding = FragmentContactBinding.inflate(inflater);
         mBinding.setLifecycleOwner(getViewLifecycleOwner());
 
-        mViewModel = new ViewModelProvider(this).get(ContactViewModel.class);
+        mViewModel = new ViewModelFactory(CalendarDatabase.getInstance(getContext()))
+                .create(ContactViewModel.class);
 
         mBinding.contactDoneButton.setOnClickListener(view -> {
-            mViewModel.addPerson(mBinding.contactNameBox.getText().toString(),mBinding.contactName2Box.getText().toString(),mBinding.contactPhoneBox.getText().toString());
+            mViewModel.addPerson(mBinding.contactNameBox.getText().toString(), mBinding.contactName2Box.getText().toString(), mBinding.contactPhoneBox.getText().toString());
             mBinding.contactNameBox.setText("");
             mBinding.contactName2Box.setText("");
             mBinding.contactPhoneBox.setText("");
