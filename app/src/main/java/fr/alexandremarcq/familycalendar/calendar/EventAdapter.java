9a81eb1adffd62522557c0a6b3e3c47d0b21f23f
alexandremarcq <alexandremarcq58@gmail.com>
2021-03-02 08:37:03 +0100

    Added RecyclerView items

---
+++ app/src/main/java/fr/alexandremarcq/familycalendar/calendar/EventAdapter.java
@@ -0,0 +1,61 @@
+package fr.alexandremarcq.familycalendar.calendar;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import fr.alexandremarcq.familycalendar.database.event.Event;
+import fr.alexandremarcq.familycalendar.databinding.RecyclerItemEventBinding;
+
+public class EventAdapter extends ListAdapter<Event, EventAdapter.EventViewHolder> {
+
+    protected EventAdapter() {
+        super(new DiffCallBack());
+    }
+
+    @NonNull
+    @Override
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new EventViewHolder(
+                RecyclerItemEventBinding.inflate(LayoutInflater.from(parent.getContext()))
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+        Event event = getItem(position);
+        holder.bind(event);
+    }
+
+    public static class DiffCallBack extends DiffUtil.ItemCallback<Event> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull Event oldItem, @NonNull Event newItem) {
+            return oldItem == newItem;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Event oldItem, @NonNull Event newItem) {
+            return oldItem.id == newItem.id;
+        }
+    }
+
+    public static class EventViewHolder extends RecyclerView.ViewHolder {
+
+        private final RecyclerItemEventBinding mBinding;
+
+        public EventViewHolder(RecyclerItemEventBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
+
+        public void bind(Event event) {
+            mBinding.setEvent(event);
+            mBinding.executePendingBindings();
+        }
+    }
+}
