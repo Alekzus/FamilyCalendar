a4091052964ed02b516f2eb9e99f66b30ac03df7
alexandremarcq <alexandremarcq58@gmail.com>
2021-03-08 10:18:43 +0100

    Small fixes

--- app/src/main/java/fr/alexandremarcq/familycalendar/addevent/AddEventFragment.java
+++ app/src/main/java/fr/alexandremarcq/familycalendar/addevent/AddEventFragment.java
@@ -112,28 +112,17 @@ public class AddEventFragment extends Fragment {
             mBinding.typeBox.setAdapter(adapter);
         });
 
-        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                  int dayOfMonth) {
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, monthOfYear);
-                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel();
-            }
-
+        DatePickerDialog.OnDateSetListener date = (view, year, monthOfYear, dayOfMonth) -> {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
         };
 
-        mBinding.dateBox.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                new DatePickerDialog(getContext(), date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-            }
-        });
+        mBinding.dateBox.setOnClickListener(v -> new DatePickerDialog(getContext(), date, myCalendar
+                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        );
 
         mViewModel.mPeople.observe(mOwner, people -> {
             ContactAdapter adapter = new ContactAdapter(
